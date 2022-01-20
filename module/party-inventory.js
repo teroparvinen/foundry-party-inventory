20ed148ff7b48571c7c70bca063fb09ab6c1482e
Tero Parvinen <tero.parvinen@gmail.com>
2022-01-20 08:41:27 +0200

    Indent using spaces

--- module/party-inventory.js
+++ module/party-inventory.js
@@ -3,41 +3,41 @@ import { addTogglePartyButton, addTogglePartyButtonTidy, addGroupInventoryIndica
 import { PartyInventory } from './inventory.js';
 
 Hooks.on('setup', () => {
-	game.settings.register(moduleId, 'scratchpad', { scope: 'world', type: Object });
+    game.settings.register(moduleId, 'scratchpad', { scope: 'world', type: Object });
 });
 
 Hooks.on('renderActorSheet5eCharacter', (sheet, html, character) => {
-	let sheetClasses = sheet.options.classes;
-	if (sheetClasses[0] === "tidy5e") {
-		addTogglePartyButtonTidy(html, sheet.actor);
-		addGroupInventoryIndicatorTidy(html, sheet.actor);
-	} else {
-		addTogglePartyButton(html, sheet.actor);
-	}
+    let sheetClasses = sheet.options.classes;
+    if (sheetClasses[0] === "tidy5e") {
+        addTogglePartyButtonTidy(html, sheet.actor);
+        addGroupInventoryIndicatorTidy(html, sheet.actor);
+    } else {
+        addTogglePartyButton(html, sheet.actor);
+    }
 });
 
 Hooks.on('getActorSheet5eCharacterHeaderButtons', (app, buttons) => {
-	buttons.unshift({
-		class: 'open-party-inventory-button',
-		icon: 'fas fa-users',
-		label: game.i18n.localize(`${localizationID}.button-title`),
-		onclick: () => {
-			PartyInventory.activate();
-		}
-	});
+    buttons.unshift({
+        class: 'open-party-inventory-button',
+        icon: 'fas fa-users',
+        label: game.i18n.localize(`${localizationID}.button-title`),
+        onclick: () => {
+            PartyInventory.activate();
+        }
+    });
 });
 
 Hooks.on('updateItem', (item) => {
-	PartyInventory.refresh();
+    PartyInventory.refresh();
 });
 
 Hooks.on('createSetting', (setting) => {
-	if (setting.key == `${moduleId}.scratchpad`) {
-		PartyInventory.refresh();
-	}
+    if (setting.key == `${moduleId}.scratchpad`) {
+        PartyInventory.refresh();
+    }
 });
 Hooks.on('updateSetting', (setting) => {
-	if (setting.key == `${moduleId}.scratchpad`) {
-		PartyInventory.refresh();
-	}
+    if (setting.key == `${moduleId}.scratchpad`) {
+        PartyInventory.refresh();
+    }
 });
--- module/sheet-inject.js
+++ module/sheet-inject.js
@@ -1,70 +1,70 @@
 import { moduleId, localizationID } from './const.js';
 
 export function addTogglePartyButton(html, actor) {
-	const enableTitle = game.i18n.localize(`${localizationID}.enable-item-title`);
-	const disableTitle = game.i18n.localize(`${localizationID}.disable-item-title`);
+    const enableTitle = game.i18n.localize(`${localizationID}.enable-item-title`);
+    const disableTitle = game.i18n.localize(`${localizationID}.disable-item-title`);
 
-	html.find(".inventory ol:not(.currency-list)  .item-control.item-edit").each(function() {
-		const currentItemId = this.closest(".item").dataset.itemId;
-		const currentItem = actor.items.find(item => item.id === currentItemId);
-		const isInPartyInventory = currentItem.getFlag(moduleId, 'inPartyInventory');
+    html.find(".inventory ol:not(.currency-list)  .item-control.item-edit").each(function() {
+        const currentItemId = this.closest(".item").dataset.itemId;
+        const currentItem = actor.items.find(item => item.id === currentItemId);
+        const isInPartyInventory = currentItem.getFlag(moduleId, 'inPartyInventory');
 
-		const title = isInPartyInventory ? disableTitle : enableTitle;
-		const active = isInPartyInventory ? 'active' : '';
+        const title = isInPartyInventory ? disableTitle : enableTitle;
+        const active = isInPartyInventory ? 'active' : '';
 
-		$(`
-			<a class="item-control party-inventory-module item-toggle ${active}" title="${title}">
-			<i class="fas fa-users"></i>
-			</a>
-		`).insertAfter(this);
+        $(`
+            <a class="item-control party-inventory-module item-toggle ${active}" title="${title}">
+            <i class="fas fa-users"></i>
+            </a>
+        `).insertAfter(this);
 
-		$(this.nextElementSibling).on('click', function() {
-			currentItem.setFlag(moduleId, 'inPartyInventory', !isInPartyInventory);
-		});
-	});
+        $(this.nextElementSibling).on('click', function() {
+            currentItem.setFlag(moduleId, 'inPartyInventory', !isInPartyInventory);
+        });
+    });
 }
 
 export function addTogglePartyButtonTidy(html, actor) {
-	const enableTitle = game.i18n.localize(`${localizationID}.enable-item-title`);
-	const disableTitle = game.i18n.localize(`${localizationID}.disable-item-title`);
+    const enableTitle = game.i18n.localize(`${localizationID}.enable-item-title`);
+    const disableTitle = game.i18n.localize(`${localizationID}.disable-item-title`);
 
-	const title = enableTitle;
+    const title = enableTitle;
 
-	html.find(".inventory .item-control.item-edit").each(function() {
-		const currentItemId = this.closest(".item").dataset.itemId;
-		const currentItem = actor.items.find(item => item.id === currentItemId);
-		const isInPartyInventory = currentItem.getFlag(moduleId, 'inPartyInventory');
+    html.find(".inventory .item-control.item-edit").each(function() {
+        const currentItemId = this.closest(".item").dataset.itemId;
+        const currentItem = actor.items.find(item => item.id === currentItemId);
+        const isInPartyInventory = currentItem.getFlag(moduleId, 'inPartyInventory');
 
-		const title = isInPartyInventory ? disableTitle : enableTitle;
-		const active = isInPartyInventory ? 'active' : '';
+        const title = isInPartyInventory ? disableTitle : enableTitle;
+        const active = isInPartyInventory ? 'active' : '';
 
-		$(`
-			<a class="item-control party-inventory-module" title="${title}">
-				<i class="fas fa-users"></i>
-				<span class="control-label">${title}</span>
-			</a>
-		`).insertAfter(this);
+        $(`
+            <a class="item-control party-inventory-module" title="${title}">
+                <i class="fas fa-users"></i>
+                <span class="control-label">${title}</span>
+            </a>
+        `).insertAfter(this);
 
-		$(this.nextElementSibling).on('click', function() {
-			currentItem.setFlag(moduleId, 'inPartyInventory', !isInPartyInventory);
-		});
-	});
+        $(this.nextElementSibling).on('click', function() {
+            currentItem.setFlag(moduleId, 'inPartyInventory', !isInPartyInventory);
+        });
+    });
 }
 
 export function addGroupInventoryIndicatorTidy(html, actor) {
-	const title = game.i18n.localize(`${localizationID}.is-in-party-inventory`);
+    const title = game.i18n.localize(`${localizationID}.is-in-party-inventory`);
 
-	html.find(".inventory .item .item-name").each(function () {
-		const currentItemId = this.closest(".item").dataset.itemId;
-		const currentItem = actor.items.find(item => item.id === currentItemId);
-		const isInPartyInventory = currentItem.getFlag(moduleId, 'inPartyInventory');
+    html.find(".inventory .item .item-name").each(function () {
+        const currentItemId = this.closest(".item").dataset.itemId;
+        const currentItem = actor.items.find(item => item.id === currentItemId);
+        const isInPartyInventory = currentItem.getFlag(moduleId, 'inPartyInventory');
 
-		if (isInPartyInventory) {
-			$(`
-				<div class="item-state-icon" title="${title}">
-					<i class="fas fa-users"></i>
-				</div>
-			`).insertAfter(this);
-		}
-	});
+        if (isInPartyInventory) {
+            $(`
+                <div class="item-state-icon" title="${title}">
+                    <i class="fas fa-users"></i>
+                </div>
+            `).insertAfter(this);
+        }
+    });
 }
